0e5042ca1a6b124d997553fc6adf16de52c60f88
sina sima <simacna@gmail.com>
2015-10-20 14:31:35 -0400

    created 2 new views, ability to save to the data base

--- blog/app/controllers/articles_controller.rb
+++ blog/app/controllers/articles_controller.rb
@@ -1,5 +1,28 @@
 class ArticlesController < ApplicationController
 
+	def index
+		@articles = Article.all
+	end
+
+	def show
+		@article = Article.find(params[:id])
+	end
+
 	def new
 	end
+
+	def create
+		# render plain: params[:article].inspect #key is plain: with value params[:article].inspect
+		# @article = Article.new(params.require(:article).permit(:title, :text)) we're creating an action since the code
+		#will be used throughout the controller
+		@article = Article.new(article_params)
+
+		@article.save
+		redirect_to @article #where is the redirect @article to?
+	end
+
+	private
+		def article_params
+			params.require(:article).permit(:title, :text)
+		end
 end

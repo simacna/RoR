1e3de738e637c5d7a4d2a152d80cfebe99a679f4
sina sima <simacna@gmail.com>
2015-10-22 11:51:32 -0400

    classes in ruby

--- rubypractice/ruby.rb
+++ rubypractice/ruby.rb
@@ -26,26 +26,134 @@ groceries = ['bananas', 'pasta', 'rice']
 
 # puts add_nums(3,4)
 
-print "Enter a number:"
-first_num = gets.to_i
-print "Enter Another:"
-second_num = gets.to_i
+# print "Enter a number:"
+# first_num = gets.to_i
+# print "Enter Another:"
+# second_num = gets.to_i
 
-begin
-	answer = first_num / second_num
+# begin
+# 	answer = first_num / second_num
 
-rescue #called if there's an error
-	puts "you can't divide by zero"
-	exit
+# rescue #called if there's an error
+# 	puts "you can't divide by zero"
+# 	exit
+# end
+
+# puts "#{first_num} / #{second_num} = #{answer}"
+# age = 12
+
+# def check_age(age)
+# 	#puts age unless age == 0 #raise ArgumentError
+	
+# 	raise ArgumentError, "Enter a positive number" unless age > 0
+# end
+
+# begin
+# 	check_age(0)
+# rescue ArgumentError
+# 	puts "That is an impossible age"
+# end
+
+# puts "Add them #{4 + 5} \n\n"
+# puts 'Add them #{4 + 5} \n\n'
+
+# multiline_string = <<EOM
+# This is a very long string that contains 
+# dgaga
+# like #{4+5} \n\n
+# EOM
+
+# puts multiline_string
+
+
+first_name = "Derek"
+last_name = "Banas"
+middle_name = "justin"
+full_name = "#{first_name} #{middle_name} #{last_name}"
+
+
+# full_name = "#{first_name} #{middle_name} #{last_name}"
+# puts full_name
+# puts full_name.include?("justin")
+
+# puts full_name.size
+# puts "Vowels: " + full_name.count("aeiou").to_s
+# puts "Consonants: " + full_name.count("^aeiou").to_s
+
+# puts full_name.start_with?("Banas")
+# puts "Index : " + full_name.index("Banas").to_s
+
+# puts "\"a\".equal(\"a\a) : " + ("a".equal?"a").to_s
+
+# puts first_name.upcase
+
+
+# puts first_name.rjust(20, '.')
+# puts first_name.ljust(20, '.')
+# puts first_name.center(20, '.')
+
+# puts full_name.chop
+# puts full_name.chomp('as')
+
+# puts full_name.delete('a')
+
+# name_array = full_name.split(//)
+# puts name_array
+
+# name_array = full_name.split(/ /)
+# puts name_array
+
+class Animal
+	def initialize #initializer, put default values
+		puts "Creating a new animal"
+	end
+
+	def set_name(new_name)
+		@name = new_name
+	end
+
+	def get_name
+		@name
+	end
+
+	def name
+		@name
+	end
+
+	def name=(new_name)
+		if new_name.is_a?(Numeric)
+			puts "Name can't be a number"
+		else
+			@name = new_name
+		end
+	end
 end
 
-puts "#{first_num} / #{second_num} = #{answer}"
+# cat =  Animal.new
+# cat.set_name("SINA")
+# puts cat.get_name
+# puts cat.name
+
+# cat.name = "Biggie"
+# puts cat.name
 
 
+class Dog
+	attr_reader :name, :height, :weight #create all getter functins
+	attr_writer :name, :height, :weight #create all setter functions
 
+	attr_accessor :name, :height, :weight #this sets both getter and setters
 
+	def bark
+		return "Generic Bark"
+	end
+end
 
+rover = Dog.new
 
+rover.name = "Rover"
+
+puts rover.name
 
 
 

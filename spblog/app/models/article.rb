15b8dc5f0382adc8e4ca2dda323e86cd48b827d2
sina sima <simacna@gmail.com>
2015-10-26 20:59:16 -0400

    creating forms

--- spblog/app/models/article.rb
+++ spblog/app/models/article.rb
@@ -1,7 +1,8 @@
 class Article < ActiveRecord::Base
 	belongs_to :user
-	
+
 	validates :title, presence: true, 
 			  length: {maximum: 50}
 	validates :post, presence: true
+	
 end
--- spblog/app/models/user.rb
+++ spblog/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+	has_many :articles
 	#want to require psw as well as email
 	validates :password, presence: true,
 			  length: {minimum: 8}

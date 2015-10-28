c8e54d5135b68ea76e2040fbe3d300c80b7c4acf
sina sima <simacna@gmail.com>
2015-10-28 17:48:03 -0400

    adding view to show comments posted along with setting up association in the comments controller to allow each comment to be associated with each article

--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -1,2 +1,19 @@
 class CommentsController < ApplicationController
+
+	def create
+		# each request for a comment has to keep track of the article to which the comment is attached, 
+		# thus the initial call to the find method of the Article model to get the article in question
+		@article = Article.find(params[:article_id]) 
+		@comment = @article.comments.create(comment_params) #create method creates and saves the comment, automatically linking the comment to the particular id
+		redirect_to article_path(@article) #This calls the show action of the ArticlesController which in turn renders the show.html.erb 
+	end
+
+	private
+		def comment_params
+			params.require(:comment).permit(:commenter, :body) 
+			# what is :comment -- is it the singular form of the :comments table?. assuming :commenter and :body come from the columns created in table
+			# with :commenter and :body
+		end
 end
+
+# create comment and route to show

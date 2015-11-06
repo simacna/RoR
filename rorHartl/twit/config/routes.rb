431675434e94c606b952963d2b7812b9ef15481e
sina sima <simacna@gmail.com>
2015-11-06 15:14:06 -0500

    set up guardfile to automatically run tests when changes are made to any of the files

--- rorHartl/twit/config/routes.rb
+++ rorHartl/twit/config/routes.rb
@@ -1,10 +1,9 @@
 Rails.application.routes.draw do
-  #root 'application#hello'
+  
 
   root 'static_pages#home'
   get 'static_pages/help'
   get 'static_pages/about'
-  get 'static_pages/contact'
 
   
   
--- rorHartl/twit/test/controllers/static_pages_controller_test.rb
+++ rorHartl/twit/test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionController::TestCase
-
   test "should get home" do
     get :home
     assert_response :success
@@ -18,12 +17,6 @@ class StaticPagesControllerTest < ActionController::TestCase
   	get :about
   	assert_response :success
   	assert_select "title", "About"
-  end
-
-  test "should get contact" do
-    get :contact
-    assert_response :success
-    assert_select "title", "Contact"
   end
 
 end

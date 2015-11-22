736c6dcf7f7ba119183087007cdcf12242e65160
sina sima <simacna@gmail.com>
2015-11-22 13:50:19 -0500

    checking to see if integration tests work as they should with the links

--- rorHartl/twit/test/integration/site_layout_test.rb
+++ rorHartl/twit/test/integration/site_layout_test.rb
@@ -5,11 +5,12 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
   #   assert true
   # end
 
+  #to check that integratio test passes, run: bundle exec rake test:integration
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
     assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", help_path
+    assert_select "a[href=?]", help_path #automatically checks for <a href="/about">..</a>
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
   end

4054f9fd5256ca11dc203c08de696d9ea315a10d
sina sima <simacna@gmail.com>
2015-11-24 18:17:24 -0500

    added bcrypt for passwords. adding minimum validations (length) for passwords

--- rorHartl/twit/test/models/user_test.rb
+++ rorHartl/twit/test/models/user_test.rb
@@ -2,11 +2,12 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: "Example User", email: "user@example.com")
+    @user = User.new(name: "Example User", email: "user@example.com",
+                     password: "foo", password_confirmation: "foo")
   end
 
   test "should be valid" do
-    assert @user.valid?    
+    assert @user.valid?
   end
 
   test "name should be present" do
@@ -52,6 +53,16 @@ class UserTest < ActiveSupport::TestCase
     duplicate_user.email = @user.email.upcase
     @user.save
     assert_not duplicate_user.valid?
+  end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = "a" * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
   end
 end
 

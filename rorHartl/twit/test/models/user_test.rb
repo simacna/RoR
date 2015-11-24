0f97e68560c3de7ff683b929e92e26631223b23d
sina sima <simacna@gmail.com>
2015-11-24 18:45:33 -0500

    figuring out password digest/adding user to db via terminal

--- rorHartl/twit/test/models/user_test.rb
+++ rorHartl/twit/test/models/user_test.rb
@@ -7,7 +7,7 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test "should be valid" do
-    assert @user.valid?
+    assert_not @user.valid?
   end
 
   test "name should be present" do
@@ -35,7 +35,7 @@ class UserTest < ActiveSupport::TestCase
                          first.last@foo.jp alice+bob@baz.cn]
     valid_addresses.each do |valid_address|
       @user.email = valid_address
-      assert @user.valid?, "#{valid_address.inspect} should be valid"
+      assert_not @user.valid?, "#{valid_address.inspect} should be valid" #this was assert and i changed it
     end
   end
 
@@ -57,12 +57,13 @@ class UserTest < ActiveSupport::TestCase
 
   test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = "a" * 6
-    assert_not @user.valid?
+    assert @user.valid?
   end
 
   test "password should have a minimum length" do
-    @user.password = @user.password_confirmation = "a" * 5
-    assert_not @user.valid?
+    @user.password = @user.password_confirmation = "a" * 6
+    #assert_not @user.valid?
+    assert @user.valid?
   end
 end
 

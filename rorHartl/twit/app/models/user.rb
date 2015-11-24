244e490102258f44f82e16ab50df56e2f07b653d
sina sima <simacna@gmail.com>
2015-11-23 20:08:46 -0500

    added failsafe to make sure unique email in db

--- rorHartl/twit/app/models/user.rb
+++ rorHartl/twit/app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  before_save { self.email = email.downcase }
+  #where does :name get passed from to the validates below? from the html name="name" form?
   validates(:name, presence: true, length: {maximum: 50}) #same as validates :name, presence: true
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates(:email, presence: true, length: {maximum: 255},

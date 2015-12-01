3ce44d26ca75e7a7a23882beeee5099f0effe767
sina sima <simacna@gmail.com>
2015-12-01 14:43:59 -0500

    adding form

--- rorHartl/twit/app/controllers/users_controller.rb
+++ rorHartl/twit/app/controllers/users_controller.rb
@@ -1,9 +1,20 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
 
   def show
     @user = User.find(params[:id])
     #debugger
   end
+
+  def create
+    @user = User.new(params[:user])
+    if @user.save
+      #handle a successful save
+    else
+      render 'new'
+    end
+  end
+
 end

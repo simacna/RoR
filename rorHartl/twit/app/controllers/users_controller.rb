2ef483dcb97edf3e57f33031bea89a74ce60c0a5
sina sima <simacna@gmail.com>
2015-12-01 14:45:27 -0500

    test

--- rorHartl/twit/app/controllers/users_controller.rb
+++ rorHartl/twit/app/controllers/users_controller.rb
@@ -11,7 +11,7 @@ class UsersController < ApplicationController
   def create
     @user = User.new(params[:user])
     if @user.save
-      #handle a successful save
+      #handle a successful saved
     else
       render 'new'
     end

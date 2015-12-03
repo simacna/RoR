37fa4653f43068cebb5c39b748cda64096a471c6
sina sima <simacna@gmail.com>
2015-12-03 16:17:16 -0500

    adding capability for user to sign up

--- rorHartl/twit/app/controllers/users_controller.rb
+++ rorHartl/twit/app/controllers/users_controller.rb
@@ -11,7 +11,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)
     if @user.save
-      #handle a successful saved
+      #currently a valid submission returns error because rails convention
+      #is to render the corresponding view for create action and there's no create view
+      flash[:success] = "Welcome!"
+      redirect_to @user 
+      #the above is the same as redirect_to user_url(@user)
     else
       render 'new'
     end
--- rorHartl/twit/test/integration/users_signup_test.rb
+++ rorHartl/twit/test/integration/users_signup_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  
   test "submission link" do 
 
   get signup_path
@@ -13,5 +14,16 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                             password_confirmation: "bar" }
     end
   assert_template 'users/new'
+  end
+
+  test "valid signup information" do
+    get signup_path
+
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path, user: { name: "Example User",
+                                            email: 'user@example.com',
+                                            password: 'password',
+                                            password_confirmation: 'password'}
+      assert_template 'users/show' #what is users/show -- also difference users/show vs users#show
   end
 end

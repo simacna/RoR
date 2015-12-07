d634efe12642897f87ea412776541cfde005a070
sina sima <simacna@gmail.com>
2015-12-07 16:28:33 -0500

    adding integration test

--- rorHartl/twit/app/controllers/sessions_controller.rb
+++ rorHartl/twit/app/controllers/sessions_controller.rb
@@ -1,4 +1,21 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      #Log the user in and redirect to the user's show page
+    else
+      #create an error message 
+      #at first i'd type flash[:erro] and the red background did not show up
+      #meaning that :danger is a keyword -- bootstrap probably!
+      flash.now[:danger] = "Invalid email/password combination" #not quite right
+      render 'new'
+    end
+  end
+
+  def destroy
+    
+  end
 end

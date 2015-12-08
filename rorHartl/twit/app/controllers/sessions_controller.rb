8c020c881a471ecc00c0d0c0858db98737d7ffda
sina sima <simacna@gmail.com>
2015-12-08 16:57:22 -0500

    changes to sessions controller

--- rorHartl/twit/app/controllers/sessions_controller.rb
+++ rorHartl/twit/app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:session][:email].downcase)
+    #what does authenticate do?
     if user && user.authenticate(params[:session][:password])
       #Log the user in and redirect to the user's show page
     else

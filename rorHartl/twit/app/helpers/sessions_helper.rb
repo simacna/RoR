baf374bbf535e8ceea7cf72790478bb74fe387db
sina sima <simacna@gmail.com>
2016-01-14 14:11:38 -0500

    pushing before continuing

--- rorHartl/twit/app/helpers/sessions_helper.rb
+++ rorHartl/twit/app/helpers/sessions_helper.rb
@@ -7,6 +7,13 @@ module SessionsHelper
     session[:user_id] = user.id
   end
 
+  #Remembers a user in a persistent session
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   #Logs out the current user
   def log_out #we'll add the method to the sessions helper module
     session.delete(:user_id)
